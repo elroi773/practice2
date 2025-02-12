8eeec258c790b67cf302d0df969117cd22464dc8
elroi773 <s2457@e-mirim.hs.kr>
2025-02-12 14:36:34 +0900

    리엑틑

---
+++ Jsx_js.js
@@ -0,0 +1,16 @@
+import ReactDom from 'react-dom';
+
+let text = 'Hello world!';
+const num = 15;
+const obj = {key : 0, a:1,b:2};
+const arr = ['a','b','c'];
+
+
+const element =(
+    <div>
+        <h1>변수 넣기</h1>
+        <ul>
+            <li>Text</li>
+        </ul>
+    </div>
+)
