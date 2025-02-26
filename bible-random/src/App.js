d0fb671711b24b4b1bc367d3d5e87206e413108c
name <s2457@e-mirim.hs.kr>
2025-02-26 23:38:30 +0900

    새벽기도회 성경구절 뜨는 영상을 코드로 만들어보기 리엑트로

---
+++ bible-random/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BibleVerse from "./components/BibleVerse";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <BibleVerse />
+    </div>
+  );
+}
+
+export default App;
