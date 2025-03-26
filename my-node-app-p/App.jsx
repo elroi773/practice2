ce96857199100ae7a17150a0ce7b6f40b6d9ec87
elroi773 <s2457@e-mirim.hs.kr>
2025-03-26 19:14:16 +0900

    Table 예제 공부

---
+++ my-node-app-p/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Table from './Table'; // Table.jsx를 임포트합니다
+
+function App() {
+  return (
+    <div className="App">
+      <Table />
+    </div>
+  );
+}
+
+export default App;
