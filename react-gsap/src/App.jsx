12e87728e3e5170271be9b73e5b371671e891a45
elroi773 <s2457@e-mirim.hs.kr>
2025-05-11 02:00:40 +0900

    애니메이션

--- react-gsap/src/App.jsx
+++ react-gsap/src/App.jsx
@@ -1,55 +1,11 @@
-import React, { useRef } from "react";
-import { gsap } from "gsap";
-import { SpiltText } from "gsap/all";
-import { useGSAP } from "@gsap/react";
-import "./App.css";
+import { TypedMessage } from "./TypedMessage";
 
-gsap.registerPlugin(SpiltText);
-
-export const TypedMessage = ({ message }) => {
-  const blockRef = useRef(null);
-  const tlRef = useRef();
-
-  useGSAP(()=>{
-    const s = new SplitText(blockRef.current,{
-      type:"lines,words",
-    });
-    const tl = gsap.timeline({delay: 0.5, repeatDelay: 0.5, repeat: -1});
-
-    tl.addLabel("enter");
-    tl.fromTo(
-      s.words, {yPercent: 100},{yPercent: 0 , ease:"circ.out",stagger:0.2},"enter"
-    );
-    tl.fromTo(
-      s.words,{opacity:0},{opacity:1,ease:"power1.out",stagger:0.2},"enter"
-    );
-    tl.addPause();
-
-    tl.to(s.words,{
-      yPercent:-200,
-      opacity:0,
-      ease:"circ.in",
-      stagger:0.1,
-      duration:0.4,
-    },);
-
-    tl.to(s.words,{
-      opacity:0,
-      ease:"power1.in",
-      stagger:0.1,
-      duration:0.4,
-    },"exit"
-  );
-    tl.to(s.words, {
-      opacity:0,
-      ease:"power1.in",
-      stagger:0.1,
-      duration: 0.4,
-    },"exit"
-  );
-  tl.Ref.current = tl;
-  },[]);
+function App() {
   return (
-    <h2 className = "message" ref = {blockRef}>{message}</h2>
-  )
+    <div className="App">
+      <TypedMessage message="Hello, GSAP World!" />
+    </div>
+  );
 }
+
+export default App;
