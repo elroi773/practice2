57e77540d60a7f217b0d88a3987e1b95e49d3324
elroi773 <s2457@e-mirim.hs.kr>
2025-05-16 13:23:38 +0900

    dropdownmenuweb

--- Dropdown-menu-web.js
+++ Dropdown-menu-web.js
@@ -4,3 +4,11 @@ const dropdownMenu = document.getElementById("dropdownMenu");
 profileBtn.addEventListener("click", () => {
   dropdownMenu.classList.toggle("hide");
 });
+
+
+document.addEventListener("click", (event) => {
+  const isClickInside = dropdownMenu.contains(event.target) || profileBtn.contains(event.target);
+  if (!isClickInside) {
+    dropdownMenu.classList.add("hide");
+  }
+});
